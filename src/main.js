f24c457579e08649c5d407b97779d7bce7e722bd
laureyap <laureyap@yahoo.com>
2020-01-01 23:56:53 +0800

    feat: add new package

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from "vue";
+
 import ViserVue from "viser-vue";
+
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
 import App from "./App";
 import router from "./router";
 import store from "./store";
@@ -7,6 +12,7 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 Vue.use(ViserVue);
+Vue.use(Antd);
 
 new Vue({
   router,
